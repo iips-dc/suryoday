b48db92887dca0855383b3b57a7c484da1a1bed6
Rahul-Sagore <rahul.sagore@gmail.com>
2014-07-10 18:06:39 +0530

    Changed index.php file, including login page

--- index.php
+++ index.php
@@ -1,68 +1,98 @@
+<!-- Including files for DB connection and Session Control -->
+<?php
+    require 'includes/login/core.inc.php';
+    require 'includes/login/connect.inc.php';
+?>
+<!-- /End of includes -->
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Suryoday Trust | Home</title>
+    <title>Suryoday | Home</title>
+    
+    <!-- Configuration for the absoulte path -->
+     <?php include "config_global.php";   ?>
+    <!-- Core Css -->
+    <?php include "includes/cssLinks.inc.php";   ?>
 
-    <!-- Links CSS files -->
-    <link rel="stylesheet" type="text/css" href="assests/js/dojo-1.9.3/dijit/themes/claro/claro.css">
-    <link rel="stylesheet" type="text/css" href="assests/css/bootstrap/bootstrap-responsive.css">
     <style>
-        body{
-            background-color: #6AC15D;
-
-        }
-    </style>
+    .text-center{
+        text-align: center;
+    }
+</style>
 </head>
 <body class="claro">
-    <div class="container">
-    	<!-- Header & Logo section -->
-    	<div class="row">
-    		<div class="col-md-4"></div>
-    		<div class="col-md-8"></div>
-    	</div>
-        <h2 style="text-align:center">Please Login To Manage Suryodoay Trust</h2>
-        <div class="row">
-            <div class="col-md-4">
+
+<!-- Including Header -->
+
+
+<?php 
+    if(loggedIn()){ #This function is in /includes/login/core.inc.pho for verfying user session
+?>
+    <div>
+        <!-- Start of page's Border Layout  -->
+        <div dojoType="dijit.layout.BorderContainer" style="width: 100%;height: 100%;"> 
+            <div dojoType="dijit.layout.ContentPane" region="top"> 
+                <?php include('includes/header.inc.php'); ?>
+                <?php include('includes/menu_bar.inc.php'); ?>
+            </div> 
+
+            <!-- Center Region of layout -->
+             <div dojoType="dijit.layout.ContentPane" id="content" region="center"> 
+                 <h3 style="color:#ED440C">Services</h3><hr>
+                <div class="row ">
+                    <div class="col-md-3 text-center">
+                        <a href="#"><img class="img-responsive img-circle" src="assests/img/baithak.jpg"></a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="public_relations"><img class="img-responsive img-circle" src="assests/img/pr.jpg"></a>
+                    </div>
+                    <div class="col-md-3">
+                         <a href="human_resource/index.php"><img class="img-responsive img-circle" src="assests/img/hr.jpg"></a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="todo"><img class="img-responsive img-circle" src="assests/img/todo.png"></a>
+                    </div>
+                </div>
+                <br><br><br><br><br><br><br><br><br>
+                <div class="row ">
+                    <div class="col-md-3 text-center">
+                        <a href="#"><img class="img-responsive img-circle" src="assests/img/projects.jpg"></a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="#"><img class="img-responsive img-circle" src="assests/img/projects.jpg"></a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="#"><img class="img-responsive img-circle" src="assests/img/projects.jpg"></a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="#"><img class="img-responsive img-circle" src="assests/img/projects.jpg"></a>
+                    </div>
+                </div>
+                <!-- .row --> 
+            </div> 
+        
+            <!-- Bottom Region of Layout -->
+            <div dojoType="dijit.layout.ContentPane" region="bottom" splitter="true"> 
+                &copy; Suryoday Trust . <span style="float:right">Powered By <a  href="#">Developement Center</a> <span>
             </div>
-            <div class="col-md-4">
-                <form action="app/index.php" name="loginForm" method="post">
-                    <label>Username </label>
-                    <input type="textbox" name="username" /><br><br>
-                    <label>Password </label>
-                    <input type="textbox" name="password" /><br>    
-                    <button type="submit" id="loginButton" dojoType="dijit.form.Button">Login</button>
-                </form>
-
-                New User?
-                <button id="registration" dojoType="dijit.form.Button">Register</button>
-                <div dojoType="dijit.layout.ContentPane" id="registrationPane"></div>
-
-            </div>
-
-            <div class="col-md-4">
-            </div>
-
         </div>
-        <!-- .row -->
+        <!-- end of Border Layout Container -->
     </div>
     <!-- .container -->
 
-    <!-- Javascript links -->
-    <script src="lib/dojo-1.9.3/dojo/dojo.js" djConfig="parseOnLoad: true"></script>
+        <?php include("includes/jsLinks.inc.php"); ?>
+        <?php include("includes/dojo_widgets.inc.php"); ?>
+        
+    <?php
+        } #End of LoggedIn function
+        else{
+            include 'includes/login/login_form.inc.php' ;
+        }
+    ?>
     <script>
         dojo.require("dijit.dijit"); //for using dojo's layout system
         dojo.require("dijit.form.Button");
-        dojo.require("dijit.layout.ContentPane");
-
-        //Binding click event to link to load html file
-        dojo.addOnLoad(function(){
-            var register = dijit.byId("registration");
-            var regPane = dijit.byId("registrationPane");
-
-            dojo.connect(register, "onClick", null, function(evt){
-                regPane.attr("href", "app/_inc/registration.inc.html");
-            })
-        });
     </script>
+
 </body>
 </html>
