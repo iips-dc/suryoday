f67db001e187b3abb5495e6f9956289fa3d11524
MY_EMAIL <porwaladisha@gmail.com>
2014-07-28 08:59:11 +0530

    UI changes

--- donation(adisha)/donation_trust.php
+++ donation(adisha)/donation_trust.php
@@ -1,8 +1,39 @@
+<!-- Including files for DB connection and Session Control -->
 <?php
-/*  Including files for DB connection and Session Control*/
-include 'includes/login/core.inc.php';
-include 'includes/login/connect.inc.php';
+    include 'includes/login/core.inc.php';
+    include 'includes/login/connect.inc.php';
+?>
+<!-- /End of includes -->
 
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Suryoday | Donation</title>
+    
+    <!-- Configuration for the absoulte path -->
+     <?php include "config_global.php";   ?>
+    <!-- Core Css -->
+    <?php include "includes/cssLinks.inc.php";   ?>
+
+</head>
+<body class="claro">
+ 
+<?php 
+    if(loggedIn()){ #This function is in /includes/login/core.inc.pho for verfying user session
+?>
+
+        <!-- Start of page's Border Layout  -->
+        <div dojoType="dijit.layout.BorderContainer" style="width: 100%;height: 100%;"> 
+            <div dojoType="dijit.layout.ContentPane" region="top" splitter="true"> 
+               <?php include('includes/header.inc.php'); ?>
+                <?php include('includes/menu_bar.inc.php'); ?>
+            </div> 
+
+            
+
+            <!-- Center Region of layout -->
+             <div dojoType="dijit.layout.ContentPane" id="content" region="center" splitter="true"> 
+             <?php
 $username = $_SESSION['username'];//session id
 /* We have to take token_id .due to absence of token genration we will insert */
 if(isset($_POST['trustDonationSubmit'])){//When submit button is clicked
@@ -96,3 +127,31 @@ if(isset($_POST['trustDonationSubmit'])){//When submit button is clicked
 }
 mysqli_close($con);
 ?>
+</div> 
+        
+            <!-- Bottom Region of Layout -->
+            <div dojoType="dijit.layout.ContentPane" region="bottom" splitter="true"> 
+                &copy; Suryoday Trust . <span style="float:right">Powered By <a  href="#">Developement Center</a> </span>
+            </div>
+        </div>
+            <!-- end of Border Layout Container -->   
+
+
+    <?php include("includes/jsLinks.inc.php"); ?> 
+    <?php include("includes/dojo_widgets.inc.php"); ?>
+
+    <!-- Script for dynamic loading of pages in center region -->
+    <script>
+        require(["dojo/parser", "dijit/MenuBar", "dijit/MenuBarItem", "dijit/PopupMenuBarItem",
+    "dijit/DropDownMenu", "dijit/MenuItem", "dijit/layout/TabContainer", "dijit/form/RadioButton", "dojo/domReady!", "dijit/form/Textarea" ]);       
+    </script>
+    <?php
+        } #End of LoggedIn function
+        else{
+            include 'includes/login/login_form.inc.php' ;
+        }
+    ?>
+
+
+</body>
+</html>
