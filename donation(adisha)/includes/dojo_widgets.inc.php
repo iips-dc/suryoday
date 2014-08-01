4ecc604c7a5ed495cc9461e80215f61c409c878a
exploreshaifali <agrawalshaifali09@gmail.com>
2014-08-01 06:07:01 +0530

    removed extra added fiels

--- donation(adisha)/includes/dojo_widgets.inc.php
+++
@@ -1,7 +0,0 @@
-<script>
-    	dojo.require("dijit.dijit");  
-        dojo.require("dijit.layout.AccordionContainer");
-        dojo.require("dijit.layout.ContentPane");
-        dojo.require("dijit.layout.BorderContainer");
-        dojo.require("dijit.layout.TabContainer");
-</script>
