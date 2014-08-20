58f9bf08961e05b464c9b964c7d55ac0964dd2e1
MY_EMAIL <porwaladisha@gmail.com>
2014-08-20 08:41:50 +0530

    added config_global_navigation file

---
+++ config_global_navigation.php
@@ -0,0 +1,19 @@
+<?php
+$index = 'index.php';
+$config_global = 'config_global.php';
+$app_index = 'app/index.php';
+$baithak_index = 'app/baithak/index.php';
+$baithak_register = 'app/baithak/register_form.php';
+$donation_index = 'app/donation/index.php';
+$human_resource_index = 'app/human_resource/index.php';
+$public_relation_index = 'app/public_relation/index.php';
+$includes_cssLinks = 'includes/cssLinks.inc.php'; 
+$includes_dojo_widgets = 'includes/dojo_widgets.inc.php';
+$includes_header = 'includes/header.inc.php';  
+$includes_jsLinks = 'includes/jsLinks.inc.php'; 
+$includes_menubar = 'includes/menu_bar.inc.php'; 
+$includes_cssLinks_inc = 'includes/cssLinks.inc.php'; 
+$login_form = 'includes/login/login_form.inc.php';
+$login_connect = 'includes/login/connect.inc.php';
+$login_core = 'includes/login/core.inc.php';
+?>
